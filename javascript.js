c2c294d044fbf62b8170c7feb695600d9c9d4737
Haddock <jhadddock@protonmail.com>
2023-02-08 12:32:52 -0800

    Add password match validation

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function checkPasswordsMatch(input) {
+    if (input.value != document.getElementById("password").value) {
+        input.setCustomValidity("Passwords do not match.")
+    }
+    
+    else {
+        input.setCustomValidity("");
+    }
+}
